d6d244d70ae17ac042204f7383a965a9dba480cd
mony <graphtheorykid@gmail.com>
2024-12-05 19:18:48 +0530

    testingthis shit

---
+++ developer.cpp
@@ -0,0 +1,7 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    cout<<"This is the start of something really special,,, this is what i wanna do for the rest of my life :)"<<endl;
+    return(0);
+}
