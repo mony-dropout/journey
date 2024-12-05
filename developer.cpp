5218bda2926a1e11ec24fa7b932980b60c148c31
mony <graphtheorykid@gmail.com>
2024-12-05 19:26:58 +0530

    does this work lmao

--- developer.cpp
+++ developer.cpp
@@ -2,6 +2,6 @@
 using namespace std;
 int main()
 {
-    cout<<"This is the start of something really special,,, this is what i wanna do for the rest of my life :)"<<endl;
+    cout<<"This is the start of something really special,,, this is what i wanna do for the rest of my life... atwu :)"<<endl;
     return(0);
 }
