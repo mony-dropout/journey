ba51aca0a23c1dd01de7af52e71f6b92e62c0e36
mony <graphtheorykid@gmail.com>
2024-12-09 01:32:30 +0530

    workkk

--- crypto.cpp
+++ crypto.cpp
@@ -12,15 +12,25 @@ string decr(string s, int j)
 }
 string encr(string s, int j)
 {
-    for (int i=0;)
+    for (int i=0;i<s.length();i++)
+    {
+        int t=int(s[i]-'a')+j;
+        t=(t+26)%26;
+        s[i]=char(int('a')+t);
+    }
+    return s;
 }
 int main()
 {
     int j; cin>>j;   
     string s;
     cin>>s; //takes input till you press ctrl+d 
+    int eord;
+    cin>>eord;
+    if (eord==1) {cout<<decr(s,j);}
+    if (eord==0) {cout<<encr(s,j);}
     
-    cout<<decr(s,j);
+    cout<<endl;
     return(0);
     
     
